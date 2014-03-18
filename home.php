4c384a0ea26e083eaf5ab0373a3a60ce7c9ac371
Pete Lower <pete.lower@gmail.com>
2014-03-18 12:43:25 -0500

    Add home page section

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Landing Page
+
+*
+* @package WordPress
+* @subpackage Twenty_Twelve
+* @since Twenty Twelve 1.0
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<section id='slider-section' class='content-section slider-section' >
+				<?php echo do_shortcode('[simplemaps]'); ?>
+			</section>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
