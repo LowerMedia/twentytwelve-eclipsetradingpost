0236d85500061131edf737379273e133d56941fc
Pete Lower <pete.lower@gmail.com>
2014-03-18 12:03:52 -0500

    Add tagline in header

--- header.php
+++ header.php
@@ -45,6 +45,7 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
 			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+			<span class="nav-slogan">Inspired trade brightens lives.</span>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 
